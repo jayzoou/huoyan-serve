fce2e6cd609fb20baa58d2e0e3780521283df81c
jayzoou <chrischau666@gmail.com>
2026-03-03 23:26:01 +0800

    feat: test

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 
@@ -26,6 +27,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.options('*', cors())
 
 app.use('/', indexRouter);
 
